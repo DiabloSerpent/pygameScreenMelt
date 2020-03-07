4b065b7e2aca981dfeb41bb99b80bea45e6b7011
DiabloSerpent <M1ddleS3c>
2020-03-06 22:57:45 -0600

    Added the old_main.py file to keep the original program easy to access, and rewrote main.py to use a ScreenMelter class/object, so that it could use an update and a draw function to act as a normal sprite.

--- main.py
+++ main.py
@@ -5,13 +5,10 @@ pygame.init()
 
 SCREENSIZE = (800, 600)
 SCREEN = pygame.display.set_mode(SCREENSIZE)
+BACKGROUND_SCREEN = pygame.Surface(SCREENSIZE)
+BACKGROUND_SCREEN.fill((0, 0, 0))
 BOUNDS = pygame.Rect((0, 0), SCREENSIZE)
 TIME = pygame.time.Clock()
-
-INITIAL_DELAY = 4 * 60
-MAX_OFFSET = 130
-MELT_SPEED = 4
-MAX_TICKS = (SCREENSIZE[1] + MAX_OFFSET + INITIAL_DELAY) // MELT_SPEED + MELT_SPEED
 
 YELLOW = pygame.Color("yellow")
 RED = pygame.Color("red")
@@ -46,38 +43,61 @@ title.draw(old_screen)
 new_screen = pygame.Surface(SCREENSIZE)
 new_screen.fill(YELLOW)
 
-column_offsets = []
-columns = []
-column = pygame.Surface((1, SCREENSIZE[1]))
-temp_old_screen = old_screen.copy()
 
-for x in range(SCREENSIZE[0]):
-    # Get a random offset for each column
-    offset = -random.randint(0, MAX_OFFSET) - INITIAL_DELAY
-    column_offsets.append(offset)
-    # Get the column as a surface
-    column.blit(temp_old_screen, (0, 0))
-    temp_old_screen.scroll(-1, 0)
-    columns.append(column.copy())
+class ScreenMelter(pygame.sprite.Sprite):
+    INITIAL_DELAY = 4 * 60
+    MAX_OFFSET = 130
+    MELT_SPEED = 4
+    MAX_TICKS = (SCREENSIZE[1] + MAX_OFFSET + INITIAL_DELAY) // MELT_SPEED
+
+    def __init__(self, old_surf, new_surf):
+        super().__init__()
+        self.old = old_surf
+        global BACKGROUND_SCREEN
+        BACKGROUND_SCREEN = new_surf
+        self.offsets = []
+        self.columns = []
+        cls = self.__class__
+        temp_old_screen = old_surf.copy()
+        column = pygame.Surface((1, SCREENSIZE[1]))
+        for x in range(SCREENSIZE[0]):
+            # Get a random offset for each column
+            offset = -random.randint(0, cls.MAX_OFFSET) - cls.INITIAL_DELAY
+            self.offsets.append(offset)
+            # Get the column as a surface
+            column.blit(temp_old_screen, (0, 0))
+            temp_old_screen.scroll(-1, 0)
+            self.columns.append(column.copy())
+
+    def update(self):
+        cls = self.__class__
+        # Shift the columns downward
+        for x in range(len(self.offsets)):
+            self.offsets[x] += cls.MELT_SPEED
+
+    def draw(self, screen):
+        for x in range(SCREENSIZE[0]):
+            if self.offsets[x] < 0:
+                # Ignore offset if it is negative, this also creates
+                # the delay effect.
+                screen.blit(self.columns[x], (x, 0))
+            else:
+                screen.blit(self.columns[x], (x, self.offsets[x]))
+
+
+melter = ScreenMelter(old_screen, new_screen)
 
 stop = False
 
-for t in range(MAX_TICKS):
+for _ in range(ScreenMelter.MAX_TICKS):
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             stop = True
     if stop:
         break
     # Reset screen
-    SCREEN.blit(new_screen, (0, 0))
-    for x in range(SCREENSIZE[0]):
-        if column_offsets[x] < 0:
-            # Ignore offset if it is negative, this also creates
-            # the delay effect.
-            SCREEN.blit(columns[x], (x, 0))
-        else:
-            SCREEN.blit(columns[x], (x, column_offsets[x]))
-        # Shift the columns downward
-        column_offsets[x] += MELT_SPEED
+    SCREEN.blit(BACKGROUND_SCREEN, (0, 0))
+    melter.update()
+    melter.draw(SCREEN)
     pygame.display.flip()
     TIME.tick(60)
